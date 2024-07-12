d0b3d1f085ac69f76c986328859af5d54e405a4d
Kurra <karrasaresh@gmail.com>
2024-07-12 16:45:39 +0530

    Route functionality is added

--- bookStoreApp/src/app/app.module.ts
+++ bookStoreApp/src/app/app.module.ts
@@ -2,15 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { HowItWorksComponent } from './how-it-works/how-it-works.component';
+import { RouterModule, Routes } from '@angular/router';
 
+export const routes: Routes = [
+  { path: 'about-us', component: AboutUsComponent },
+  { path: 'how-it-works', component: HowItWorksComponent },
+];
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, AboutUsComponent, HowItWorksComponent],
+  imports: [BrowserModule, RouterModule.forRoot(routes)],
+  exports: [RouterModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
